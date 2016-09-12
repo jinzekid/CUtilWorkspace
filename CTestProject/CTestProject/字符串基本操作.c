c31dbf865d94e5345953980a609bd40e1b66ded4
JasonLu <514432996@qq.com>
2016-09-12 19:24:49 +0800

    添加getMem方法

--- CTestProject/CTestProject/字符串基本操作.c
+++ CTestProject/CTestProject/字符串基本操作.c
@@ -74,6 +74,55 @@ char * trimSpace(char * inBuf, char ** outBuf) {
     return tmpRetBuf;
 }
 
+//指针做输出，被调用函数分配内存－－OK
+//指针做输入，主调用函数分配内存
+//求文件中国年的两端话的长度
+int getMem(char ** p1, int * len1, char ** p2, int * len2) {
+    char * tmp1 = NULL;
+    tmp1 = (char *)malloc(100);
+    if (tmp1 == NULL)
+        return -1;
+    
+    strcpy(tmp1, "abcdefg");
+    *len1 = strlen(tmp1);
+    
+    *p1 = tmp1;
+    
+    char * tmp2 = NULL;
+    tmp2 = (char *)malloc(100);
+    if (tmp2 == NULL)
+        return -1;
+    
+    strcpy(tmp2, "111222333");
+    *len2 = strlen(tmp2);
+    
+    *p2 = tmp2;
+    
+    return 0;
+}
+
+//释放内存
+int getMem_Free(char ** p1) {
+    char * tmp = NULL;
+    if (p1 == NULL)
+        return -1;
+    
+    tmp = *p1;
+    free(tmp); //释放完指针变量所指向的内存空间
+    *p1 = NULL;//把实参修改成null
+    
+    return 0;
+}
+
+//这样也可以，但是不能把实参修改成null
+int getMem_Free0(char * p1) {
+    if (p1 == NULL)
+        return -1;
+    
+    free(p1);
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
     
     // insert code here...
@@ -82,17 +131,36 @@ int main(int argc, const char * argv[]) {
 //    revserString(buf, to);
 //    printf("to===%s\n", to);
     
-    char buf[] = " adbdfd ";
-    char * retBuf = NULL;
-    char * retBuf2 = NULL;
-    printf("before trim space==%s\n", buf);
-    printf("length==%ld\n", strlen(buf));
-    printf("sizeof==%ld\n", sizeof(buf));
-    retBuf = trimSpace(buf, &retBuf2);
-    printf("after trim space==%s\n", retBuf);
-    printf("after trim space==%s\n", retBuf2);
-    printf("length==%ld\n", strlen(retBuf));
-    printf("sizeof==%ld\n", sizeof(retBuf));
+//    char buf[] = " adbdfd ";
+//    char * retBuf = NULL;
+//    char * retBuf2 = NULL;
+//    printf("before trim space==%s\n", buf);
+//    printf("length==%ld\n", strlen(buf));
+//    printf("sizeof==%ld\n", sizeof(buf));
+//    retBuf = trimSpace(buf, &retBuf2);
+//    printf("after trim space==%s\n", retBuf);
+//    printf("after trim space==%s\n", retBuf2);
+//    printf("length==%ld\n", strlen(retBuf));
+//    printf("sizeof==%ld\n", sizeof(retBuf));
+    
+    char * p1 = NULL;
+    int len1 = 0;
+    
+    char * p2 = NULL;
+    int len2 = 0;
+    
+    int ret = 0;
+    ret = getMem(&p1, &len1, &p2, &len2);
+    
+    printf("p1==%s\n", p1);
+    printf("p2==%s\n", p2);
+    
+    getMem_Free(&p1);
+    getMem_Free0(p2); //在被调用函数中，把p2所指向的内存给释放掉，但是实参p1不能被修改成null,有野指针现象
+    
+    printf("p1==%s\n", p1);
+    printf("p2==%s\n", p2);
+    
     printf("hello...\n");
     return 0;
 }
